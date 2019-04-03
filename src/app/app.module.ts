8a174447e76a9cc4d8502ce8b54ae545dcd76e3c
Geoffrey Sandja <geoffreysandja@gmail.com>
2019-04-03 15:58:03 +0530

    Adding Metric

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MetricComponent } from './metric/metric.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    MetricComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
