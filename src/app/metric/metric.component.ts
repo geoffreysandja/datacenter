8c3d0e45696202885ce50883c40ebc1c92fe8d21
Geoffrey Sandja <geoffreysandja@gmail.com>
2019-04-04 15:35:10 +0530

    Adding Modal

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,ChangeDetectionStrategy,OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-metric',
   templateUrl: './metric.component.html',
-  styleUrls: ['./metric.component.css']
+  styleUrls: ['./metric.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MetricComponent implements OnInit {
+export class MetricComponent implements OnInit,OnChanges {
   //@Input() title: string='';
   //@Input() description: string='';
   private _value: number=0;
@@ -23,6 +24,10 @@ export class MetricComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  ngOnChanges(changes){
+    if(changes.value && isNaN(changes.value.currentValue)) this.value=0;
+    if(changes.max && isNaN(changes.max.currentValue)) this.max=0;
   }
   isDanger(){
     return this.value/this.max > 0.7;
--- src/app/nodes-row/nodes-row.component.ts
+++ src/app/nodes-row/nodes-row.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NodesDetailComponent } from '../nodes-detail/nodes-detail.component';
 
 @Component({
   selector: '[app-nodes-row]',
@@ -7,11 +9,15 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class NodesRowComponent implements OnInit {
   @Input() node:any;
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
   isDanger(prop){
     return this.node[prop].used / this.node[prop].available > 0.7;
   }
   ngOnInit() {
+  }
+  open(node){
+    const modal= this.modalService.open(NodesDetailComponent);
+    modal.componentInstance.node=node;
   }
 
 }
